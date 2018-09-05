5ecbc32530e322c8eede276a99a8b9b0d09e1440
Mayank Sharma <ms8295111175@gmail.com>
2018-09-05 17:52:14 +0530

    Assignment9

---
+++ Assignment9/Substr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Substr {
+  public static void main(String[] args) {
+    
+    String str="xyz";
+    System.out.println("Substring of xyz:");
+       System.out.println(str.substring(0));
+       System.out.println(str.substring(1));
+       System.out.println(str.substring(0,2));
+       System.out.println(str.substring(2));
+       System.out.println(str.substring(1,2));
+       System.out.println(str.substring(0,1));
+  }
+}
